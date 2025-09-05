7287fb30c1e4f157c376cbf3c58dc2c63fcb0e7f
EmiRoberti77 <emiroberti@icloud.com>
2025-09-05 07:24:42 +0100

    streams

---
+++ rtsp_streamer/start_stream_4.sh
@@ -0,0 +1,11 @@
+echo "Running start_stream_1.sh"
+
+IP=$(ip route get 8.8.8.8 | awk '{print $7}')
+echo "IP: $IP"
+
+ffmpeg -re -stream_loop -1 -i videos/emi_test.mp4 \
+  -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv420p \
+  -c:a aac -ar 44100 -b:a 128k -ac 2 \
+  -f rtsp -rtsp_transport tcp rtsp://$IP:8554/mystream_4
+
+echo "Stream 4 started"
