665708c8f5bbab538f26553b759d262c9540dd5f
EmiRoberti77 <emiroberti@icloud.com>
2025-09-22 06:57:34 +0100

    updated grpc clients to support model_name

--- ray_actors/grpc_client_start.py
+++ ray_actors/grpc_client_start.py
@@ -2,9 +2,20 @@ import grpc
 from generated import server_commands_pb2_grpc as pb2_grpc
 from generated import server_commands_pb2 as pb2
 
+models = {
+    "model_oaix_box":"oaix_medicine_v1.pt",
+    "model_yolo_11_m":"yolo11m.pt",
+}
+
 _WEBHOOK = "http://192.168.1.188:3000/webhooks/camera"
 _INPUT = "rtsp://127.23.23.15:8554/mystream_4"
 _NAME = "channel_1"
+_MODEL_NAME = models.get("model_oaix_box")
+
+print(f"{_WEBHOOK=}")
+print(f"{_INPUT=}")
+print(f"{_NAME=}")
+print(f"{_MODEL_NAME=}")
 
 
 def main(host: str = "localhost", port: int = 50051) -> None:
@@ -19,6 +30,7 @@ def main(host: str = "localhost", port: int = 50051) -> None:
             frame_orientation=pb2.FrameOrientation.PORTRAIT,
             rotation=pb2.Rotation.ROTATE_90,
             processor_type=pb2.ProcessorType.GPU,
+            model_name=_MODEL_NAME
         )
 
         req = pb2.ExecuteCommandRequest(execute_commands=[channel_1])
