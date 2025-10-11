7f22debb9607f08e5382ff8e1272a8c56c127dc5
EmiRoberti77 <emiroberti@icloud.com>
2025-10-11 11:10:34 +0100

    ffmpeg-video rotation

--- rtsp_streamer/start_stream_5.sh
+++ rtsp_streamer/start_stream_5.sh
@@ -1,9 +1,9 @@
-echo "Running start_stream_1.sh"
+echo "Running start_stream_5.sh"
 
 IP=$(ip route get 8.8.8.8 | awk '{print $7}')
 echo "IP: $IP"
 
-ffmpeg -re -stream_loop -1 -i videos/emi_test.mp4 \
+ffmpeg -re -stream_loop -1 -i videos/Medicinas_rotated_180_2.mp4 \
   -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv420p \
   -c:a aac -ar 44100 -b:a 128k -ac 2 \
   -f rtsp -rtsp_transport tcp rtsp://$IP:8554/mystream_5
